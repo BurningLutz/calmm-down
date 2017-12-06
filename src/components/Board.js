cf2fd795feda23cff633b42a6c1f7cf8c42cd11b
Lutz <lutz.l.burning@gmail.com>
2017-12-07 00:19:04 +0800

    a better Board impl

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,21 +9,26 @@ import s from './Board.scss'
 
 export default function Board({game}) {
   const squares = game.view('squares')
-  const isXNext = game.view('isXNext')
-  const onClick = value => () => U.holding(() => {
-    value.view(L.choose(v => {
-      console.log(111, v)
-      return ''
-    })).get(10)
-    value.set(isXNext.get() ? 'X' : 'O')
-    isXNext.modify(R.not)
+  const onClick = i => () => game.modify(x => {
+    if (x.squares[i] === undefined) {
+      return U.seq(
+        x,
+        L.set(['squares', i], x.isXNext ? 'X': 'O'),
+        L.modify('isXNext', R.not)
+      )
+    } else {
+      return x
+    }
   })
 
   return (
     <div className={s.board}>
-      {U.mapElems(
-        (value, i) => <Square key={i} value={value} onClick={onClick(value)} />,
-        squares
+      {U.seq(
+        squares,
+        U.indices,
+        U.mapCached(i =>
+          <Square key={i} value={squares.view(i)} onClick={onClick(i)} />
+        )
       )}
     </div>
   )
